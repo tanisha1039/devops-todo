1e862a580b9398f31bbddabbe5bf50849fe8ea0b
tanisha d <tanishad@tanishas-MacBook-Air.local>
2026-05-21 10:24:19 +0530

    inducing error3

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const path = require('node:path');
+//const express = require('express');
+//const path = require('node:path');
 
 const app = express();
 app.use(express.json());
